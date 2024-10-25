d0a096d0f5f7e1ad37a577cc24d31298189d6a72
Rangel Ivanov <rangel.ivanov@sap.com>
2024-10-25 15:02:28 +0300

    Simplify anonymization filter

--- anonymization/app.py
+++ anonymization/app.py
@@ -3,70 +3,22 @@ import cv2
 import subprocess
 import sys
 import errno
+import numpy as np
 
 from face_anon_filter import FaceAnonymizer
 
-input_fifo = '/tmp/video_input_fifo'
-output_fifo = '/tmp/video_output_fifo'
 
-if not os.path.exists(input_fifo):
-    os.mkfifo(input_fifo)
-if not os.path.exists(output_fifo):
-    os.mkfifo(output_fifo)
+processor = FaceAnonymizer()
+# TODO: extract from env
+width, height = 1920, 1080
 
-# Start ffmpeg subprocess to handle stdin -> FIFO
-ffmpeg_read_cmd = [
-    'ffmpeg',
-    '-i', '-',              # Read from stdin
-    '-f', 'rawvideo',       # Output raw video
-    '-pix_fmt', 'bgr24',    # Format OpenCV expects
-    input_fifo              # Output to FIFO
-]
-ffmpeg_read_process = subprocess.Popen(ffmpeg_read_cmd, stdin=subprocess.PIPE)
+while True:
+    in_bytes = sys.stdin.buffer.read(width * height * 3)
+    if not in_bytes:
+        break
 
-# Start ffmpeg subprocess to handle FIFO -> stdout
-ffmpeg_write_cmd = [
-    'ffmpeg',
-    '-f', 'rawvideo',
-    '-pix_fmt', 'bgr24',
-    '-s', '640x480',        # Adjust based on input resolution
-    '-r', '30',             # Frame rate
-    '-i', output_fifo,      # Read from output FIFO
-    '-vcodec', 'libx264',
-    '-f', 'mp4',
-    '-'  # Write to stdout
-]
-ffmpeg_write_process = subprocess.Popen(ffmpeg_write_cmd, stdin=subprocess.PIPE, stdout=sys.stdout)
+    frame = np.frombuffer(in_bytes, np.uint8).reshape((height, width, 3))
 
-# OpenCV Capture and Processing
-cap = cv2.VideoCapture(input_fifo)  # Use FIFO for input
+    processed_frame = processor(frame)
 
-# Initialize VideoWriter for the output FIFO
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(output_fifo, fourcc, 30, (640, 480))  # Adjust FPS and resolution
-video_filter = FaceAnonymizer()
-
-while cap.isOpened():
-    try:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        processed_frame = video_filter(frame)
-
-        # Write processed frame to the output FIFO (non-blocking)
-        out.write(processed_frame)
-
-    except IOError as e:
-        if e.errno == errno.EAGAIN:
-            # If FIFO is not ready, continue looping without blocking
-            continue
-
-# Cleanup
-cap.release()
-out.release()
-
-ffmpeg_read_process.stdin.close()
-ffmpeg_write_process.stdin.close()
-ffmpeg_read_process.wait()
-ffmpeg_write_process.wait()
+    sys.stdout.buffer.write(processed_frame.tobytes())
