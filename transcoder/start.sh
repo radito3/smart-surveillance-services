af86a47f0139071d01e0c13b5cee6336b60dbab0
Rangel Ivanov <rangel.ivanov@sap.com>
2024-10-25 14:14:44 +0300

    Fix transcoder start script

--- transcoder/start.sh
+++ transcoder/start.sh
@@ -14,7 +14,8 @@ ffmpeg -i "$1" -filter_complex \
 -map "[v2out]" -b:v:1 1400k -g 48 -keyint_min 48 -sc_threshold 0 -hls_time 4 \
   -hls_segment_filename /var/www/hls/854p_%03d.ts -f hls /var/www/hls/854p.m3u8 \
 -map "[v3out]" -b:v:2 2500k -g 48 -keyint_min 48 -sc_threshold 0 -hls_time 4 \
-  -hls_segment_filename /var/www/hls/1280p_%03d.ts -f hls /var/www/hls/1280p.m3u8-map "[v1out]" -b:v:0 800k -g 48 -keyint_min 48 -f dash /var/www/dash/dash_640p.mpd \
+  -hls_segment_filename /var/www/hls/1280p_%03d.ts -f hls /var/www/hls/1280p.m3u8 \
+-map "[v1out]" -b:v:0 800k -g 48 -keyint_min 48 -f dash /var/www/dash/dash_640p.mpd \
 -map "[v2out]" -b:v:1 1400k -g 48 -keyint_min 48 -f dash /var/www/dash/dash_854p.mpd \
 -map "[v3out]" -b:v:2 2500k -g 48 -keyint_min 48 -f dash /var/www/dash/dash_1280p.mpd \
 -map "[v4out]" -b:v:3 3000k -f rtsp rtsp://localhost:8554/live.sdp \
