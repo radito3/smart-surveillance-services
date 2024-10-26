302ab4c94e3f5314d4874d78a83f566e9ed5e445
Rangel Ivanov <rangel.ivanov@sap.com>
2024-10-26 10:48:10 +0300

    Update notification service

--- notifications/notification.go
+++ notifications/notification.go
@@ -1,7 +1,8 @@
-package notifications
+package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,46 +16,103 @@ import (
 	"github.com/go-chi/chi/middleware"
 )
 
+type RequestData struct {
+	CameraID  string           `json:"camera_id,omitempty"`
+	Receivers []ReceiverConfig `json:"receivers,omitempty"`
+}
+
 type ReceiverConfig struct {
-	UIPopup       bool   `json:"ui_popup,omitempty"`
+	UIPopup       bool   `json:"ui_popup,omitempty"` // default
 	SmtpServer    string `json:"smtp_server,omitempty"`
-	SmtpRecipient string
+	SmtpRecipient string `json:"smtp_recipient,omitempty"`
 	// ...
 }
 
 var receivers map[string][]ReceiverConfig = make(map[string][]ReceiverConfig)
 
-func createReceiver(w http.ResponseWriter, r *http.Request) {
-	// TODO: read from request body the type of receivers
-	// the camera ID will be in the path
+func addCameraConfig(writer http.ResponseWriter, request *http.Request) {
+	defer request.Body.Close()
+
+	var data RequestData
+	err := json.NewDecoder(request.Body).Decode(&data)
+	if err != nil {
+		http.Error(writer, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	if len(data.CameraID) == 0 {
+		http.Error(writer, "Missing required Camera ID", http.StatusBadRequest)
+		return
+	}
+
+	_, present := receivers[data.CameraID]
+	if present {
+		http.Error(writer, fmt.Sprintf("Camera %s already configured", data.CameraID), http.StatusConflict)
+		return
+	}
+	receivers[data.CameraID] = data.Receivers
+	writer.WriteHeader(http.StatusCreated)
 }
 
-func sendNotification(w http.ResponseWriter, r *http.Request) {
-	log.Printf("received alert signal from {camera ID}")
-	// TODO: send to all receivers
-	_ = receivers["a"]
+func updateCameraConfig(writer http.ResponseWriter, request *http.Request) {
+	cameraID := chi.URLParam(request, "cameraID")
+	defer request.Body.Close()
+
+	var data RequestData
+	err := json.NewDecoder(request.Body).Decode(&data)
+	if err != nil {
+		http.Error(writer, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	_, present := receivers[cameraID]
+	if !present {
+		http.Error(writer, fmt.Sprintf("Camera %s not found", cameraID), http.StatusNotFound)
+		return
+	}
+	receivers[cameraID] = data.Receivers
+	writer.WriteHeader(http.StatusOK)
 }
 
-// TODO: do not bother with a full mailing implementation, just write about it in the thesis paper
-func sendEmail() {
+func sendNotification(writer http.ResponseWriter, request *http.Request) {
+	cameraID := chi.URLParam(request, "cameraID")
+	log.Printf("received alert signal from %s", cameraID)
+
+	config, present := receivers[cameraID]
+	if !present {
+		http.Error(writer, fmt.Sprintf("Camera %s not found", cameraID), http.StatusNotFound)
+		return
+	}
+
+	for _, receiver := range config {
+		// TODO: impl
+		log.Printf("sending alert to %v", receiver)
+
+		if len(receiver.SmtpServer) != 0 {
+			if err := sendEmail(cameraID, receiver); err != nil {
+				http.Error(writer, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+	}
+	writer.WriteHeader(http.StatusOK)
+}
+
+func sendEmail(cameraID string, config ReceiverConfig) error {
 	//  https://myaccount.google.com/apppasswords
 	//  Navigate to App Password Generator, designate an app name such as "security project," and obtain a 16-digit password.
 	//  Copy this password and paste it into the designated password field as instructed.
-
 	auth := smtp.PlainAuth("", "john.doe@gmail.com", "extremely_secret_pass", "smtp.gmail.com")
 
-	to := []string{"kate.doe@example.com"}
+	to := []string{config.SmtpRecipient}
 
 	msg := []byte("To: kate.doe@example.com\r\n" +
 		"Subject: Security Alert\r\n" +
 		"\r\n" +
-		"Camera {N} has detected suspicios behaviour\r\n")
+		"Camera " + cameraID + " has detected suspicious behaviour\r\n")
 
-	err := smtp.SendMail("smtp.gmail.com:587", auth, "john.doe@gmail.com", to, msg)
-
-	if err != nil {
-		log.Println(err)
-	}
+	//"smtp.gmail.com:587"
+	return smtp.SendMail(config.SmtpServer, auth, "john.doe@gmail.com", to, msg)
 }
 
 func main() {
@@ -69,11 +127,12 @@ func main() {
 	router.Use(middleware.CleanPath)
 	router.Use(middleware.Heartbeat("/public/ping"))
 
-	router.Post("/add/{cameraID}", sendNotification)
-	router.Post("/alert", sendNotification)
+	router.Post("/add", addCameraConfig)
+	router.Patch("/update/{cameraID}", updateCameraConfig)
+	router.Post("/alert/{cameraID}", sendNotification)
 
 	server := &http.Server{
-		Addr:              ":80",
+		Addr:              ":8080",
 		Handler:           router,
 		ReadTimeout:       5 * time.Second,
 		ReadHeaderTimeout: 2 * time.Second,
@@ -83,7 +142,6 @@ func main() {
 
 	go func() {
 		log.Println("starting server...")
-		// TODO: make an self-signed SSL certificate
 		err := server.ListenAndServe()
 		if err != nil && err != http.ErrServerClosed {
 			fmt.Fprintln(os.Stderr, err.Error())
